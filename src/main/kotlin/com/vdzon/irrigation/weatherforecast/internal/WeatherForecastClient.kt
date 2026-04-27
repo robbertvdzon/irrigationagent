65ae2ab24b95d8798c45692bc6b26112cc7d40b9
Robbert vd Zon <robbert.vanderzon@edsn.nl>
2026-04-27 10:05:27 +0200

    Irrigation agent

--- src/main/kotlin/com/vdzon/irrigation/weatherforecast/internal/WeatherForecastClient.kt
+++ src/main/kotlin/com/vdzon/irrigation/weatherforecast/internal/WeatherForecastClient.kt
@@ -1,5 +1,10 @@
 package com.vdzon.irrigation.weatherforecast.internal
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter
+import io.github.resilience4j.retry.annotation.Retry
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter
 import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 
@@ -12,5 +17,10 @@ data class ForecastResponse(
 @HttpExchange("/forecast")
 interface WeatherForecastClient {
     @GetExchange
+    @Retry(name = "weatherForecast")
+    @CircuitBreaker(name = "weatherForecast")
+    @RateLimiter(name = "weatherForecast")
+    @TimeLimiter(name = "weatherForecast")
+    @Bulkhead(name = "weatherForecast")
     suspend fun getDailyForecast(): ForecastResponse
 }
